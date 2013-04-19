519c756ebe037443be21fe5c6b3180f0526fce44
David Gageot <david@gageot.net>
2013-04-19 09:20:06 +0200

    White background favicon

--- _bin/favicon.sh
+++ _bin/favicon.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-convert $1 -resize 256x256! -transparent white favicon-256.png
+convert logo.png -resize 256x256! -transparent white favicon-256.png
 
 for size in 16 32 64 128; do
 	convert favicon-256.png -resize ${size}x${size} favicon-${size}.png
 done
 
-convert favicon-{16,32,64,128,256}.png +dither -colors 256 favicon.ico
+convert favicon-{16,32,64,128,256}.png +dither -colors 256 -transparent white favicon.ico
 
 convert favicon-256.png -resize 57x57 apple-touch-icon.png
 for size in 72 114; do
